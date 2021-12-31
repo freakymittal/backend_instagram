4f82fd8d3a96bacd279f1b2c3727028a5564571c
Howard Wolowitz <bosepriyangshu2001@gmai.com>
2021-12-31 21:25:32 +0530

    Modifications 2.0.1

--- backend/src/api/Controllers/middleware/Auth/auth.js
+++ backend/src/api/Controllers/middleware/Auth/auth.js
@@ -1,6 +1,6 @@
 const jwt=require('jsonwebtoken')
 const mongoose=require('mongoose')
-const User=require('../../../../Schema/users')
+const User=require('../../../../models/users')
 const {tokenPublicKey}=require('../../config/env')
 module.exports=async (req,res,next)=>{
      const {authorization}=req.headers
